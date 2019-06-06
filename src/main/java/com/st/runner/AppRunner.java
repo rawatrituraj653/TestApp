0585dc2be0a62f2fb08a50160332c903816fcd92
admin <admin@192.168.0.13>
2019-06-06 20:20:19 +0530

    test

---
+++ src/main/java/com/st/runner/AppRunner.java
@@ -0,0 +1,14 @@
+package com.st.runner;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class AppRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("AppRunner");
+
+	}
+
+}
